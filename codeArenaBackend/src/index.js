4b2cb9a69002f84c30d1064c8abd8c585033383c
vishalrathore8oct <vishal.rathore8oct@gmail.com>
2025-04-23 10:28:25 +0530

    setup besic express server and .env file and install dotenv

---
+++ codeArenaBackend/src/index.js
@@ -0,0 +1,14 @@
+import app from './app.js';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const PORT = process.env.PORT || 4000;
+
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
+
+app.listen(PORT, () => {
+  console.log(`🌐 Server is running on URL http://127.0.0.1:${PORT}`);
+});
