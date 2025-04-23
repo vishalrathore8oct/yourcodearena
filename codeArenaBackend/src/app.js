03dfba1b83a7b2fa36d8f9192d65fd1bb98a2636
vishalrathore8oct <vishal.rathore8oct@gmail.com>
2025-04-23 11:32:31 +0530

    install winston & morgon and cofigure a logger utility file in utils folder and use a morgan middleware in express app

--- codeArenaBackend/src/app.js
+++ codeArenaBackend/src/app.js
@@ -1,5 +1,25 @@
 import express from 'express';
+import morgan from 'morgan';
+import logger from './utils/logger.utils.js';
 
 const app = express();
+const morganFormat = ":method :url :status :response-time ms";
+
+// Middleware to log requests
+app.use(
+    morgan(morganFormat, {
+      stream: {
+        write: (message) => {
+          const logObject = {
+            method: message.split(" ")[0],
+            url: message.split(" ")[1],
+            status: message.split(" ")[2],
+            responseTime: message.split(" ")[3],
+          };
+          logger.info(JSON.stringify(logObject));
+        },
+      },
+    })
+  );
 
 export default app;
