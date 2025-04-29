334e4c3dc2721296c59b1103ef4f074fb76e0613
vishalrathore8oct <vishal.rathore8oct@gmail.com>
2025-04-29 15:08:12 +0530

    Add middlewares for cors, cookies, body json data and url params data

--- codeArenaBackend/src/app.js
+++ codeArenaBackend/src/app.js
@@ -3,12 +3,18 @@ import morgan from 'morgan';
 import logger from './utils/logger.utils.js';
 import { swaggerUi, swaggerSpec } from './utils/swagger.utils.js';
 import { errorHandler } from './middlewares/errorHandler.middlewares.js';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 
 const morganFormat = ":method :url :status :response-time ms";
 
-
+// Add All Essetial Middlewares
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(cookieParser())
 
 
 // Middleware to log requests
